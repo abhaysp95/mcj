ea02fd2157a0b2bfd470a03770e102ca7cf859ae
coolabhays <abhaysp9955@gmail.com>
2020-12-07 19:30:46 +0530

    handling login event

--- src/com/mcj/Controller.java
+++ src/com/mcj/Controller.java
@@ -1,5 +1,31 @@
 package com.mcj;
 
-class Controller {
-        /** code */
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+
+public class Controller {
+	@FXML private TextField enteredName;
+	@FXML private TextField enteredAddress;
+	@FXML private TextField enteredPort;
+	@FXML private ActionEvent event;
+	@FXML private Button loginButton;
+
+	public void perfomLogin(ActionEvent event) {
+		this.event = event;
+		String username = enteredName.getText();
+		String address = enteredAddress.getText();
+		int port = Integer.parseInt(enteredPort.getText());
+		login(username, address, port);
+	}
+
+	private void login(String username, String address, int port) {
+		((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
+		System.out.println("Name: " + username + "\n"
+				+ "Address: " + address + "\n"
+				+ "Port: " + port);
+	}
 }
