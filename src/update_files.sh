1f2ca7ff00e70de3976ae36f304a66561bc644f2
coolabhays <abhaysp9955@gmail.com>
2020-12-07 18:54:32 +0530

    update script to update files other than of java

---
+++ src/update_files.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+rsync /opt/SceneBuilder/app/login.fxml ~/Downloads/git-materials/my_repos/mcj/src/com/mcj
+rsync /opt/SceneBuilder/app/login.fxml ~/Downloads/git-materials/my_repos/mcj/bin/com.mcj/com/mcj
+
+
+rsync ~/Downloads/git-materials/my_repos/mcj/src/com/mcj/login.fxml /opt/SceneBuilder/app
+rsync ~/Downloads/git-materials/my_repos/mcj/src/com/mcj/login.fxml ~/Downloads/git-materials/my_repos/mcj/bin/com.mcj/com/mcj
