6ea40c56d7d63ec3ba643a3815b36a06c516c3d1
coolabhays <abhaysp9955@gmail.com>
2020-12-07 21:04:56 +0530

    sync client.fxml

--- src/update_files.sh
+++ src/update_files.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
+# login.fxml
 rsync /opt/SceneBuilder/app/login.fxml ~/Downloads/git-materials/my_repos/mcj/src/com/mcj
 rsync /opt/SceneBuilder/app/login.fxml ~/Downloads/git-materials/my_repos/mcj/bin/com.mcj/com/mcj
 
+# client.fxml
+rsync /opt/SceneBuilder/app/client.fxml ~/Downloads/git-materials/my_repos/mcj/src/com/mcj
+rsync /opt/SceneBuilder/app/client.fxml ~/Downloads/git-materials/my_repos/mcj/bin/com.mcj/com/mcj
 
+
+# login.fxml
 rsync ~/Downloads/git-materials/my_repos/mcj/src/com/mcj/login.fxml /opt/SceneBuilder/app
 rsync ~/Downloads/git-materials/my_repos/mcj/src/com/mcj/login.fxml ~/Downloads/git-materials/my_repos/mcj/bin/com.mcj/com/mcj
+
+# client.fxml
+rsync ~/Downloads/git-materials/my_repos/mcj/src/com/mcj/client.fxml /opt/SceneBuilder/app
+rsync ~/Downloads/git-materials/my_repos/mcj/src/com/mcj/client.fxml ~/Downloads/git-materials/my_repos/mcj/bin/com.mcj/com/mcj
